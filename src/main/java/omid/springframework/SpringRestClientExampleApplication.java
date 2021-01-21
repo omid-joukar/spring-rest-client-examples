b07413b4ec265aada4be0674d45dd1065b4ca331
omid-joukar <joukaromid6@gmail.com>
2021-01-21 09:11:53 +0100

    initial project add.

---
+++ src/main/java/omid/springframework/SpringRestClientExampleApplication.java
@@ -0,0 +1,13 @@
+package omid.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestClientExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestClientExampleApplication.class, args);
+    }
+
+}
